5d4a7cb02529114c76e22f825ef9b01282f89027
Aude-Christy <feukamaude55@gmail.com>
2019-08-19 23:20:11 +0200

    git commit

--- src/Entity/planning.php
+++ src/Entity/planning.php
@@ -7,28 +7,5 @@ class Planning
     /**
      * @ManyToone(targetEntity="Interventions", mappedBy="Planning")
      */
-    private $Interventions;
-
-    public function --construct()
-    {
-        
-        $this->$Interventions = new ArrayCollection();
-    }
-
-    public function addInterventions(Interventions $Interventions)
-    {
-        $this->Interventions[] = $Interventions;
-        return $this;
-
-        $entityManager->persist(interventions);
-        $entityManager->flush();
-    }
-
-    public getInterventions()
-    {
-        return $this->Interventions;
-
-    }
-    
     private $date_planning;
 }
